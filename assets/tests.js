cb9442c2707a96c2f6aa39d5cf1237c780847fbb
Evgeniy Khaberev <ehaberev@yandex.ru>
2022-06-22 12:56:58 +0000

    Deploy to GitHub Pages

---
+++ assets/tests.js
@@ -0,0 +1,162 @@
+'use strict';
+
+define('book/tests/app.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | app');
+
+  QUnit.test('app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/env.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/env.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('router.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/index.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/index.js should pass ESLint\n\n');
+  });
+});
+define('book/tests/integration/helpers/env-test', ['qunit', 'ember-qunit', '@ember/test-helpers'], function (_qunit, _emberQunit, _testHelpers) {
+  'use strict';
+
+  function _asyncToGenerator(fn) {
+    return function () {
+      var gen = fn.apply(this, arguments);
+      return new Promise(function (resolve, reject) {
+        function step(key, arg) {
+          try {
+            var info = gen[key](arg);
+            var value = info.value;
+          } catch (error) {
+            reject(error);
+            return;
+          }
+
+          if (info.done) {
+            resolve(value);
+          } else {
+            return Promise.resolve(value).then(function (value) {
+              step("next", value);
+            }, function (err) {
+              step("throw", err);
+            });
+          }
+        }
+
+        return step("next");
+      });
+    };
+  }
+
+  (0, _qunit.module)('Integration | Helper | env', function (hooks) {
+    (0, _emberQunit.setupRenderingTest)(hooks);
+
+    // Replace this with your real tests.
+    (0, _qunit.test)('it renders', function () {
+      var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(assert) {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                this.set('inputValue', '1234');
+
+                _context.next = 3;
+                return (0, _testHelpers.render)(Ember.HTMLBars.template({
+                  "id": "gK7RAtd+",
+                  "block": "{\"symbols\":[],\"statements\":[[1,[26,\"env\",[[22,[\"inputValue\"]]],null],false]],\"hasEval\":false}",
+                  "meta": {}
+                }));
+
+              case 3:
+
+                assert.equal(this.element.textContent.trim(), '1234');
+
+              case 4:
+              case 'end':
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+
+      return function (_x) {
+        return _ref2.apply(this, arguments);
+      };
+    }());
+  });
+});
+define('book/tests/test-helper', ['book/app', 'book/config/environment', '@ember/test-helpers', 'ember-qunit'], function (_app, _environment, _testHelpers, _emberQunit) {
+  'use strict';
+
+  (0, _testHelpers.setApplication)(_app.default.create(_environment.default.APP));
+
+  (0, _emberQunit.start)();
+});
+define('book/tests/tests.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | tests');
+
+  QUnit.test('integration/helpers/env-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'integration/helpers/env-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('test-helper.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/routes/index-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/index-test.js should pass ESLint\n\n');
+  });
+});
+define('book/tests/unit/routes/index-test', ['qunit', 'ember-qunit'], function (_qunit, _emberQunit) {
+  'use strict';
+
+  (0, _qunit.module)('Unit | Route | index', function (hooks) {
+    (0, _emberQunit.setupTest)(hooks);
+
+    (0, _qunit.test)('it exists', function (assert) {
+      var route = this.owner.lookup('route:index');
+      assert.ok(route);
+    });
+  });
+});
+define('book/config/environment', [], function() {
+  var prefix = 'book';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+require('book/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
