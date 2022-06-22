8d6b885333e668fa84a420f058b8c08fdef418de
IvanSyAlex <74009917+IvanSyAlex@users.noreply.github.com>
2022-06-22 07:42:35 +0500

    change rootUrl

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'book',
     environment,
-    rootURL: '/',
+    rootURL: '/ember/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
